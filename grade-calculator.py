ecf95fac3e8a5b7a49762b1e9831f488d4e38695
Jerrett Longworth <jerrett7@gmail.com>
2018-03-25 16:39:59 -0400

    clean up code and cover exceptions

--- grade-calculator.py
+++ grade-calculator.py
@@ -1,112 +1,121 @@
-import time
-
 def isint(x):
     try:
         int(x)
         return True
-    except:
+    except ValueError:
         return False
+
 
 def isfloat(x):
     try:
         float(x)
         return True
-    except:
+    except ValueError:
         return False
 
+
+def isindex(name, index):
+    try:
+        name[index]
+        return True
+    except IndexError:
+        return False
+
+
+def append_to_array(array_name, x, prompt):
+    while not isindex(array_name, x):
+        input_value = input(prompt)
+        if isfloat(input_value):
+            array_name.append(float(input_value))
+
+
 def calculate_categorized():
-    # we need to ask how many categories we're about to collect data for
+    # Initialize variables
+    total = 0
     number_of_categories = ""
+    weight = []
+    points_achieved = []
+    points_total = []
+    global final_grade
+
+    # we need to ask how many categories we're about to collect data for
     while not isint(number_of_categories):
         input_value = input("What is the number of grade categories? ")
         if isint(input_value):
             number_of_categories = int(input_value)
 
-    # Initialize variables
-    total = 0
-    weight = []
-    points_achieved = []
-    points_total = []
-
     # Time to ask about the category's weight, and points out of points total
-    for x in range(number_of_categories):
-        weight_input = float(input("[" + str(x + 1) + "/" + str(number_of_categories) + "] % weight of category #" + str(x + 1) + "? "))
-        if isfloat(weight_input):
-            weight.append(weight_input)
-        else:
-            print("This is not a valid number.")
-            exit()
+    # append_to_array() takes the name of the array, the index, and what
+    # prompt to show while waiting to get a proper value
+    for i in range(number_of_categories):
+        append_to_array(weight, i, "[" + str(i + 1) + "/" +
+                        str(number_of_categories) + "] % weight of category #"
+                        + str(i + 1) + "? ")
 
-        points_achieved_input = float(input("[" + str(x + 1) + "/" + str(number_of_categories) + "] Points achieved in category #" + str(x + 1) + "? "))
-        if isfloat(points_achieved_input):
-            points_achieved.append(points_achieved_input)
-        else:
-            print("This is not a valid number.")
-            exit()
+        append_to_array(points_achieved, i, "[" + str(i + 1) + "/" +
+                        str(number_of_categories) + "] Points achieved in " +
+                        "category #" + str(i + 1) + "? ")
 
-        points_total_input = float(input("[" + str(x + 1) + "/" + str(number_of_categories) + "] Points total in category #" + str(x + 1) + "? "))
-        if isfloat(points_total_input):
-            points_total.append(points_total_input)
-        else:
-            print("This is not a valid number.")
-            exit()
+        append_to_array(points_total, i, "[" + str(i + 1) + "/" +
+                        str(number_of_categories) + "] Points total in " +
+                        "category #" + str(i + 1) + "? ")
 
+        # Space after every category
         print()
 
-    for x in range(number_of_categories):
-        value = float(points_achieved[x] / points_total[x] * weight[x] / 100)
-        total = total + value
+    # Calculate the total grade by taking the sum of the percents from each
+    # category
+    for i in range(number_of_categories):
+        weighted_category = float(points_achieved[i] / points_total[i] *
+                                  weight[i])
+        total = total + weighted_category
 
-    print("Your calculated grade is: " + str(total * 100) + "%")
-
-# print("Hey there! ever wanted to see how bad you're failing in school", end="\r")
-# time.sleep(2)
-
-# print("Hey there! ever wanted to calculate your grades               ")
-# time.sleep(2)
-
-# first ask which function we're dealing with
-input_text = input("\nSelect the type of class:" + "\n(1) Categorized" +
-                   "\n(2) Uncategorized\n\n")
+    final_grade = total
 
 
-if input_text == "1":
-    class_type = "categorized"
-elif input_text == "2":
-    class_type = "uncategorized"
-else:
-    print("wrong")
-    exit()
+def calculate_uncategorized():
+    # Initialize variables
+    points_achieved = 0
+    points_total = 0
+    global final_grade
 
-
-
-if class_type == "categorized":
-    calculate_categorized()
-
-elif class_type == "uncategorized":
-    # we only need points/total
+    # we only need points and total
     print("Enter all the achieved points from each assignment separated with" +
           " enter: \n" +
           "Type anything that isn't a number when you are done.\n")
-    achieved_points = 0
-    total = 0
     x = True
-    while x == True:
+    while x is True:
         value = input("")
         if isfloat(value):
-            achieved_points = achieved_points + float(value)
+            points_achieved = points_achieved + float(value)
         else:
             x = False
 
-    print("\nNow enter the maximum possible scores of each assignment separated with enter:\n")
+    print("\nNow enter the maximum possible scores of each assignment " +
+          "separated with enter:\n")
     x = True
-    while x == True:
+    while x is True:
         value = input("")
         if isfloat(value):
-           total = total + float(value)
+            points_total = points_total + float(value)
         else:
             x = False
 
-    print("Your calculated grade is: " + str(achieved_points / total * 100) + "%")
+    try:
+        final_grade = points_achieved / points_total * 100
+    except ZeroDivisionError:
+        final_grade = 0
 
 
+# first ask which function we're dealing with
+class_type = ""
+while class_type != "1" and class_type != "2":
+    class_type = input("\nSelect the type of class:" + "\n(1) Categorized" +
+                       "\n(2) Uncategorized\n\n")
+
+if class_type == "1":
+    calculate_categorized()
+elif class_type == "2":
+    calculate_uncategorized()
+
+print("\nYour calculated grade is: " + str(final_grade) + "%")
